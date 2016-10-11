23b362eba888ca8adbb3d9896ea9ed097e6a8609
Toon Willems <willemstoon@gmail.com>
2016-10-11 15:32:38 +0000

    more readable euler-5

--- euler-5/src/main.rs
+++ euler-5/src/main.rs
@@ -1,22 +1,26 @@
 extern crate primes;
 
 use primes::PrimeInfo;
+use std::collections::HashMap;
 
 fn main() {
-    let mut common_divisors: Vec<u64> = Vec::new();
+    let mut common_divisors: HashMap<u64, u64> = HashMap::new();
+
     for x in 2..21 {
-        let mut i = 0;
-        for p in (x as u64).prime_factors() {
-            match common_divisors.iter().skip(i).position(|v| *v == p) {
-                Some(new_i) => i += new_i + 1,
-                None => {
-                    common_divisors.push(p);
-                    i = 0
-                }
+        let prime_fact = (x as u64).prime_factors();
+        let mut count_map: HashMap<u64, u64> = HashMap::new();
+        for &x in prime_fact.iter() {
+            *count_map.entry(x).or_insert(0) += 1
+        }
+
+        for (k, v) in count_map {
+            let entry = common_divisors.entry(k).or_insert(v);
+            if *entry < v {
+                *entry = v
             }
         }
     }
 
-    let result: u64 = common_divisors.iter().product();
+    let result: u64 = common_divisors.iter().map(|(k, v)| k.pow(*v as u32)).product();
     println!("{}", result);
 }
