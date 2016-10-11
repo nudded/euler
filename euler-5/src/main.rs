fe0ba367e5a801fceb52f7704808cf8dde29fb08
Toon Willems <willemstoon@gmail.com>
2016-10-11 15:01:51 +0000

    add solution for euler 5

--- euler-5/src/main.rs
+++ euler-5/src/main.rs
@@ -1,3 +1,22 @@
+extern crate primes;
+
+use primes::PrimeInfo;
+
 fn main() {
-    println!("Hello, world!");
+    let mut common_divisors: Vec<u64> = Vec::new();
+    for x in 2..21 {
+        let mut i = 0;
+        for p in (x as u64).prime_factors() {
+            match common_divisors.iter().skip(i).position(|v| *v == p) {
+                Some(new_i) => i += new_i + 1,
+                None => {
+                    common_divisors.push(p);
+                    i = 0
+                }
+            }
+        }
+    }
+
+    let result: u64 = common_divisors.iter().product();
+    println!("{}", result);
 }
